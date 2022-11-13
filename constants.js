deb12e762ead49f8651314708a515c629ce2cded
hansdav <hdavidson21@gmail.com>
2022-11-13 13:43:10 +0100

    added words for second difficulty

--- constants.js
+++ constants.js
@@ -53,12 +53,65 @@ const HANGMAN_PICS = [
   // TODO: Fill this list with values about a certain topic
   // you are passionate about: e.g. famous scientists, chess players, ...
   const WORDS_TO_GUESS = [
+    [
     'CHINA',
     'JAPAN',
     'AUSTRIA',
     'BELGIUM',
-    'ENGLAND'
-  ];
+    'ENGLAND',
+    'POLAND',
+    'SWITZERLAND',
+    'SPAIN',
+    'MALAYSIA',
+    'NIGERA',
+    'EGYPT',
+    'BRAZIL',
+    'AUSTRALIA',
+    'ARGENTINA',
+    'PARAGUAY',
+    'MEXICO',
+    'ESTONIA',
+    'SWEDEN',
+    'DENMARK',
+    'ALGERIA',
+    'FINLAND',
+    'IRAN',
+    'RUSSIA',
+    'UKRAINE',
+    'INDIA'
+  ],
+  [
+    'TOKYO',
+    'VIENNA',
+    'WARSAW',
+    'STOCKHOLME',
+    'PARIS',
+    'BEIJING',
+    'CAIRO',
+    'BERLIN',
+    'OSLO',
+    'OTTAWA',
+    'MADRID',
+    'CANBERRA',
+    'SEOUL',
+    'PYONGYANG',
+    'BRUSSELS',
+    'HELSINKI',
+    'BRASILIA',
+    'LONDON',
+    'LIMA',
+    'EDINBURGH',
+    'DUBLIN',
+    'ZURICH',
+    'PRAGUE',
+    'BRATISLAVA',
+    'BUDAPEST',
+    'BANGKOK',
+    'ANKARA',
+    'MANILA',
+    'HANOI'
+  ]
+    ];
   
   module.exports = {
     HANGMAN_PICS: HANGMAN_PICS,
